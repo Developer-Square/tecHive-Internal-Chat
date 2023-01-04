1937fc0d3191720dc5a558f7f08d06ffce64d725
ryann254 <keddelyronjoz@gmail.com>
2023-01-04 11:57:42 +0300

    feat: complete react setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
-function App() {
+import './App.css';
+import { ChannelContainer, ChannelListContainer } from './components';
+
+const client = StreamChat.getInstance(process.env.REACT_APP_API_KEY || '');
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app__wrapper'>
+      <Chat client={client} theme='team light'>
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
-}
+};
 
 export default App;
